ff7327494319c6a9391a815b351052aba4c44528
zygrade009 <vigneshcheepurapalli@gmail.com>
2024-11-14 11:28:22 +0530

    adding assets

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../styles/footer.css"
+function Footer() {
+  return (
+    <div className="footer-container">
+        <div className="footer-title"> Copyright 2024 Vignesh</div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/header.css"
+function Header() {
+  return (
+    <div className="header-container">
+        <img src="src\assets\logo.svg" alt="logo" />
+        <div className="header-title">Memory Master</div>
+    </div>
+  )
+}
+
+export default Header
